330dcac1c129d249f30280ca5cfb12a92c649aa9
phuc.phannguyenhoai@gmail.com <phuc.phannguyenhoai@gmail.com>
2023-06-25 22:29:09 +0700

    up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
-import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import AuthProviders from "./AuthProviders";
+
 import { getCurrentUser } from "@/lib/session";
+
+import AuthProviders from "./AuthProviders";
+import Button from "./Button";
+import { NavLinks } from "@/constants";
 
 const Navbar = async () => {
   const session = await getCurrentUser();
@@ -12,21 +14,25 @@ const Navbar = async () => {
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
         <Link href="/">
-          <Image src={"./logo.svg"} width={115} height={43} alt="logo" />
+          <Image src="/logo.svg" width={116} height={43} alt="logo" />
         </Link>
         <ul className="xl:flex hidden text-small gap-7">
           {NavLinks.map((link) => (
-            <Link href={link.href} key={link.key}>
+            <Link href={link.href} key={link.text}>
               {link.text}
             </Link>
           ))}
         </ul>
       </div>
+
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
-            <Link href={"/create-project"}>Share work</Link>
+            {/* <ProfileMenu session={session} /> */}
+
+            <Link href="/create-project">
+              <Button title="Share work" />
+            </Link>
           </>
         ) : (
           <AuthProviders />
